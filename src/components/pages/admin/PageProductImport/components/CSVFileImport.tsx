18f401d2ab8dac975dcc5c3120e73e929b7c32d7
Anton Fiadotauu <anton_fiadotau@epam.com>
2024-07-12 11:30:10 +0300

    07_authorization

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import axios from "axios";
+import { getAuthorizationTokenFromLocalStorage } from "~/utils/utils";
+import ErrorContext from "~/context/context";
 
 type CSVFileImportProps = {
   url: string;
@@ -9,6 +11,7 @@ type CSVFileImportProps = {
 };
 
 export default function CSVFileImport({ url, title }: CSVFileImportProps) {
+  const { setError } = useContext(ErrorContext);
   const [file, setFile] = React.useState<File>();
 
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -25,26 +28,45 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
 
   const uploadFile = async () => {
     console.log("uploadFile to", url);
+    console.log(getAuthorizationTokenFromLocalStorage());
 
     // Get the presigned URL
     // @ts-ignore
-    const response = await axios({
-      method: "GET",
-      url,
-      params: {
-        // @ts-ignore
-        name: encodeURIComponent(file.name)
+    const headers = getAuthorizationTokenFromLocalStorage();
+
+    try {
+      const response = await axios({
+        headers: headers ? headers : undefined,
+        method: "GET",
+        url,
+        params: {
+          // @ts-ignore
+          name: encodeURIComponent(file.name)
+        }
+      });
+
+      // @ts-ignore
+      console.log("File to upload: ", file.name);
+
+      const result = await fetch(response.data.signedUrl, {
+        method: "PUT",
+        body: file
+      });
+      console.log("Result: ", result);
+      setFile(undefined);
+
+    } catch (error: any) {
+      console.log(`==> Error while send a file <==`);
+      console.log(error);
+      console.log(error.response.data.message);
+
+      if (error.response && error.response.data) {
+        console.log(error.response.data.message);
+        setError({ errorCode: error.response.status, errorMessage: error.response.data.message });
       }
-    });
-    // @ts-ignore
-    console.log("File to upload: ", file.name);
-    const result = await fetch(response.data.signedUrl, {
-      method: "PUT",
-      body: file
-    });
-    console.log("Result: ", result);
-    setFile(undefined);
+    }
   };
+
   return (
     <Box>
       <Typography variant="h6" gutterBottom>
