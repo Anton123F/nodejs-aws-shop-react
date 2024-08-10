11913d81f1493fb737f2077c424e61363563fcf9
Anton Fiadotauu <anton_fiadotau@epam.com>
2024-08-10 16:52:04 +0300

    10_backend_for_frontend

--- src/mocks/data.ts
+++ src/mocks/data.ts
@@ -9,36 +9,43 @@ export const products: Product[] = [
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
     price: 24,
     title: "ProductOne",
+    imageURL: "example"
+
   },
   {
     description: "Short Product Description7",
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
     price: 15,
     title: "ProductTitle",
+    imageURL: "example"
   },
   {
     description: "Short Product Description2",
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
     price: 23,
     title: "Product",
+    imageURL: "example"
   },
   {
     description: "Short Product Description4",
     id: "7567ec4b-b10c-48c5-9345-fc73348a80a1",
     price: 15,
     title: "ProductTest",
+    imageURL: "example"
   },
   {
     description: "Short Product Descriptio1",
     id: "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
     price: 23,
     title: "Product2",
+    imageURL: "example"
   },
   {
     description: "Short Product Description7",
     id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
     price: 15,
     title: "ProductName",
+    imageURL: "example"
   },
 ];
 
@@ -53,6 +60,7 @@ export const cart: CartItem[] = [
       id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
       price: 24,
       title: "ProductOne",
+      imageURL: "example"
     },
     count: 2,
   },
@@ -62,6 +70,7 @@ export const cart: CartItem[] = [
       id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
       price: 15,
       title: "ProductName",
+      imageURL: "example"
     },
     count: 5,
   },
